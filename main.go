6d4fbcdb0fa1a9ac06872162d95523c7dcd83228
agustin <agusella@fi.uba.ar>
2023-05-03 17:16:03 -0300

    Creación de un nuevo package "ordenamiento" donde cree un quicksort para los padrones de los votantes.

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package tp1
+package rerepolez
 
 import (
 	"bufio"
@@ -12,6 +12,8 @@ func main() {
 
 	//Esto supongo que ira en un archivo go en el que implementamos todo el sistema de votos llamando a las primitivas y todo eso
 	// como es para leer un archivo segun apuntes de la catedra
+
+	s := bufio.Scanner{}
 	archivo, err := os.Open(ruta)
 	if err != nil {
 		fmt.Printf("Archivo %s no encontrado", ruta)
@@ -19,12 +21,27 @@ func main() {
 	}
 	defer archivo.Close()
 
-	s := bufio.NewScanner(archivo)
-	for s.Scan() {
-		fmt.Printf("Leí: %s\n", s.Text())
+	onComma := func(data []byte, atEOF bool) (advance int, token []byte, err error) {
+		for i := 0; i < len(data); i++ {
+			if data[i] == ',' {
+				return i + 1, data[:i], nil
+			}
+		}
+		if !atEOF {
+			return 0, nil, nil
+		}
+
+		err = s.Err()
+		if err != nil {
+			fmt.Println(err)
+		}
+		return 0, data, bufio.ErrFinalToken
 	}
-	err = s.Err()
-	if err != nil {
-		fmt.Println(err)
+	a := bufio.NewScanner(os.Stdin)
+	a.Split(onComma)
+	columna := 0
+	for a.Scan() {
+		fmt.Printf("Leí: %s\n", a.Text())
+		fmt.Printf("De la columna: %v \n", columna)
 	}
 }
---
+++ ordenamiento/ordenarVotantes.go
@@ -0,0 +1,44 @@
+package ordenamiento
+
+import (
+	TDAVotante "rerepolez/votos"
+)
+
+// es un quickSort: recive un array de votantes (primitivas leerdni votar-deshacer-finvoto) (dni-yavoto-votosrealizados)
+func OrdenarPadrones(arr []TDAVotante.Votante) []TDAVotante.Votante {
+	//caso base
+	if len(arr) == 1 {
+		return arr
+	}
+	//caso general
+	medio := len(arr) / 2
+	pivote := 0
+	ultimoMenor := 0
+	//acordarse de al swapear, pasar la dirección
+	swap(&arr[pivote], &arr[medio]) // esto dicen es para optimizar
+
+	for i := pivote + 1; i < len(arr); i++ {
+		if arr[i].LeerDNI() < arr[pivote].LeerDNI() {
+			ultimoMenor++
+			swap(&arr[i], &arr[ultimoMenor]) //aca voy acomodando todos los menores al pivote en fila
+		}
+	}
+	//coloco al elemento del pivote al final de la fila de los menores al pivote y al ultimomenor en la primera posición
+	swap(&arr[ultimoMenor], &arr[pivote])
+
+	//hago el llamado a la recursividad para que se ordenen las mitades menores al elemento del pivote y mayores al mismo
+	//acordarse que los slices no incluyen al final
+	OrdenarPadrones(arr[0 : ultimoMenor+1]) //hago el llamado para la mitad de menores hasta donde está el pivote
+	OrdenarPadrones(arr[ultimoMenor+2:])    //hago el llamado para la mitad superior al pivote.
+	return nil
+}
+
+func swap(a *TDAVotante.Votante, b *TDAVotante.Votante) {
+	//capaz sale si hago que se pase como parametro la dirección
+	*a, *b = *b, *a
+
+	/*votanteAux := TDAVotante.CrearVotante(a.LeerDNI())
+	  a = b
+	  b = votanteAux*/
+	// con esto no es que cambio exactamente, sino que sobreescribo lo que tenia en cada posición
+}
