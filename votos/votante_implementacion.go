a67668cda753fa86120eeded635fa8805cede4db
NicoCoronelCaz <108557059+NicoCoronelCaz@users.noreply.github.com>
2023-05-07 17:23:04 -0300

    Update votante_implementacion.go

--- votos/votante_implementacion.go
+++ votos/votante_implementacion.go
@@ -1,66 +1,65 @@
 package votos
 
-import (
-	"rerepolez/errores"
-	TDAPila "tdas/pila"
-)
-
 type votanteImplementacion struct {
-	dni             int
-	yaVoto          bool
-	votosRealizados TDAPila.Pila[Voto]
+    dni    int
+    yaVoto bool
+    votosRealizados pilaDinamica[Voto]
 }
 
 func CrearVotante(dni int) Votante {
-	nuevoVoto := new(votanteImplementacion)
-	nuevoVoto.votosRealizados = TDAPila.CrearPilaDinamica[Voto]()
-	nuevoVoto.dni = dni
-	return nuevoVoto
+    nuevoVoto := new(votanteImplementacion)
+    nuevoVoto.votosRealizados = CrearPilaDinamica[Voto]()
+    return nuevoVoto
 }
 
 func (votante votanteImplementacion) LeerDNI() int {
-	return votante.dni
+    return votante.dni
 }
 
 func (votante *votanteImplementacion) Votar(tipo TipoVoto, alternativa int) error {
-	//Si ya voto devuelvo el error
-	if votante.yaVoto {
-		return errores.ErrorVotanteFraudulento{Dni: votante.dni}
-	}
-	//Aca creo un nuevo voto, copio el anterior y modifico lo que corresponde
-	voto := votante.votosRealizados.VerTope()
-	voto.VotoPorTipo[tipo] = alternativa
-	votante.votosRealizados.Apilar(voto)
+    //Si ya voto devuelvo el error
+    if votante.yaVoto {
+        votante.votosRealizados.Apilar(new(Voto))
+        return ErrorVotanteFraudulento{Dni: votante.dni}
+    }
 
-	return nil
+    //Aca creo un nuevo voto, copio el anterior y modifico lo que corresponde
+    voto := votante.votosRealizados.VerTope()
+    voto.VotoPorTipo[tipo] = alternativa
+    if alternativa == 0 {
+        voto.Impugnado = true
+    }
+    votante.votosRealizados.Apilar(voto)
+
+    return nil
 
 }
 
 func (votante *votanteImplementacion) Deshacer() error {
-	//Si no hay votos, imprime el error
-	if votante.votosRealizados.EstaVacia() {
+    //Si no hay votos, imprime el error
+    if votante.votosRealizados.EstaVacia() {
+        return ErrorNoHayVotosAnteriores{}
+    }
+    //Si ya voto
+    if votante.yaVoto {
+        votante.votosRealizados.Apilar(new(Voto))
+        return ErrorVotanteFraudulento{Dni: votante.dni}
+    }
 
-		return errores.ErrorNoHayVotosAnteriores{}
-	}
-	//Si ya voto
-	if votante.yaVoto {
-		fraude := errores.ErrorVotanteFraudulento{Dni: votante.dni}
-		fraude.Error() // capaz no hay que hacer esto sino que en el ma-in si recivimos un error != nil habria que aplicar el metodo Error()
-		return fraude
-	}
+    //Elimino la version del ultimo voto
+    votante.votosRealizados.Desapilar()
 
-	//Elimino la version del ultimo voto
-	votante.votosRealizados.Desapilar()
 
-	return nil
+    return nil
 }
 
 func (votante *votanteImplementacion) FinVoto() (Voto, error) {
-	if votante.yaVoto {
-		fraude := errores.ErrorVotanteFraudulento{Dni: votante.dni}
-		fraude.Error()
-		return Voto{}, fraude
-	}
+    if votante.yaVoto {
+        votante.votosRealizados.Apilar(new(Voto))
+        return Voto{}, ErrorVotanteFraudulento{Dni: votante.dni}
+    }
 
-	return votante.votosRealizados.VerTope(), nil
+    votante.yaVoto = true
+
+    return votante.votosRealizados.VerTope(), nil
 }
