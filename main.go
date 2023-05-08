5555a4d3e28c8017505676c649b7b071203f6032
NicoCoronelCaz <108557059+NicoCoronelCaz@users.noreply.github.com>
2023-05-08 12:05:49 -0300

    Update main.go
    Faltaría borrar los comentarios y quizá poner una que otra constante

--- main.go
+++ main.go
@@ -1,123 +1,128 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
-    TDAOrdenYBusqueda "rerepolez/ordenamiento"
-    TDAVotos "rerepolez/votos"
-    "strings"
-    TDACola "tdas/cola"
-    //"strings"
-    //TDACola "tdas/cola"
-    "strconv"
-    "rerepolez/errores"
+	"bufio"
+	"fmt"
+	"os"
+	TDAOrdenYBusqueda "rerepolez/ordenamiento"
+	TDAVotos "rerepolez/votos"
+	"strings"
+	TDACola "tdas/cola"
+	//"strings"
+	//TDACola "tdas/cola"
+	"rerepolez/errores"
+	"strconv"
 )
 
 func main() {
-
-    padrones, errdni := os.Open(os.Args[1])
-    partidos, errPartidos := os.Open(os.Args[2])
-
-    if errdni != nil {
-        errAbrir := new(errores.ErrorLeerArchivo)
-        fmt.Println(errAbrir.Error())
+    if len(os.Args) != 3 {
+        errParametros := new(errores.ErrorParametros)
+        fmt.Println(errParametros.Error())
         return
     }
-    defer padrones.Close()
 
-    d := bufio.NewScanner(padrones)
-    nroVotantes := 0
-    arrayVotantes := []TDAVotos.Votante{}
+	padrones, errdni := os.Open(os.Args[2])
+	partidos, errPartidos := os.Open(os.Args[1])
 
-    for d.Scan() {
-        dni, errAtoi := strconv.Atoi(d.Text())
-        votante := TDAVotos.CrearVotante(dni)
+	if errdni != nil {
+		errAbrir := new(errores.ErrorLeerArchivo)
+		fmt.Println(errAbrir.Error())
+		return
+	}
+	defer padrones.Close()
 
-        arrayVotantes = append(arrayVotantes, votante)
+	d := bufio.NewScanner(padrones)
+	nroVotantes := 0
+	arrayVotantes := []TDAVotos.Votante{}
 
-        nroVotantes++
-        if errAtoi != nil {
-            fmt.Println(errAtoi)
-        }
-    }
+	for d.Scan() {
+		dni, errAtoi := strconv.Atoi(d.Text())
+		votante := TDAVotos.CrearVotante(dni)
 
-    errdni = d.Err()
-    if errdni != nil {
-        fmt.Println(errdni)
-    }
-    TDAOrdenYBusqueda.OrdenarPadrones(arrayVotantes)
-    /*for i := 0; i < nroVotantes; i++ {
-        fmt.Println(arrayVotantes[i].LeerDNI())
-    }*/
+		arrayVotantes = append(arrayVotantes, votante)
 
-    p := bufio.NewScanner(partidos)
+		nroVotantes++
+		if errAtoi != nil {
+			fmt.Println(errAtoi)
+		}
+	}
 
-    if errPartidos != nil {
-        errAbrir := new(errores.ErrorLeerArchivo)
-        fmt.Println(errAbrir.Error())
-    }
+	errdni = d.Err()
+	if errdni != nil {
+		fmt.Println(errdni)
+	}
+	TDAOrdenYBusqueda.OrdenarPadrones(arrayVotantes)
+	/*for i := 0; i < nroVotantes; i++ {
+	    fmt.Println(arrayVotantes[i].LeerDNI())
+	}*/
 
-    defer partidos.Close()
+	p := bufio.NewScanner(partidos)
 
-    //creo array de partidos.
-    //puedo hacerlo una lista. lo dejo comentado esta opción
-    //La ventaja que veo de hacer listas es que guardar los partidos es O(1)
-    //ya que la primitiva insertar es de orden cte mientras que append ni idea, creo es O(n).
+	if errPartidos != nil {
+		errAbrir := new(errores.ErrorLeerArchivo)
+		fmt.Println(errAbrir.Error())
+	}
 
-    //listaPartidos := TDALista.CrearListaEnlazada[TDAVotos.Partido]()
-    //listaPartidos.InsertarPrimero(TDAVotos.CrearVotosEnBlanco())
-    arrPartidos := []TDAVotos.Partido{}
-    arrPartidos = append(arrPartidos, TDAVotos.CrearVotosEnBlanco())
+	defer partidos.Close()
 
-    for p.Scan() {
-        linea := p.Text()
-        nombresPartidos := strings.Split(linea, ",")
-        nombre := nombresPartidos[0]
-        candidatos := [3]string{nombresPartidos[1], nombresPartidos[2], nombresPartidos[3]}
-        partidoCreado := TDAVotos.CrearPartido(nombre, candidatos)
-        //listaPartidos.InsertarUltimo(partidoCreado)
-        arrPartidos = append(arrPartidos, partidoCreado)
-    }
-    errPartidos = p.Err()
-    if errPartidos != nil {
-        fmt.Println(errPartidos)
-    }
+	//creo array de partidos.
+	//puedo hacerlo una lista. lo dejo comentado esta opción
+	//La ventaja que veo de hacer listas es que guardar los partidos es O(1)
+	//ya que la primitiva insertar es de orden cte mientras que append ni idea, creo es O(n).
 
-    scanner := bufio.NewScanner(os.Stdin)
-    colaIngresantes := TDACola.CrearColaEnlazada[TDAVotos.Votante]()
+	//listaPartidos := TDALista.CrearListaEnlazada[TDAVotos.Partido]()
+	//listaPartidos.InsertarPrimero(TDAVotos.CrearVotosEnBlanco())
+	arrPartidos := []TDAVotos.Partido{}
+	arrPartidos = append(arrPartidos, TDAVotos.CrearVotosEnBlanco())
 
-    contadorImpugnados := 0
-    //For que termina cuando finaliza entrada estandar
+	for p.Scan() {
+		linea := p.Text()
+		nombresPartidos := strings.Split(linea, ",")
+		nombre := nombresPartidos[0]
+		candidatos := [3]string{nombresPartidos[1], nombresPartidos[2], nombresPartidos[3]}
+		partidoCreado := TDAVotos.CrearPartido(nombre, candidatos)
+		//listaPartidos.InsertarUltimo(partidoCreado)
+		arrPartidos = append(arrPartidos, partidoCreado)
+	}
+	errPartidos = p.Err()
+	if errPartidos != nil {
+		fmt.Println(errPartidos)
+	}
 
-    //hace la llamada en cada linea, para analizar los comandos.
-    //Habria que definir su ComandosLeidos devuelve algo(un error) o es void
-    for scanner.Scan() {
-        linea := scanner.Text()
-        TDAOrdenYBusqueda.ComandosLeidos(linea, colaIngresantes, arrayVotantes, arrPartidos, &contadorImpugnados)
-    }
+	scanner := bufio.NewScanner(os.Stdin)
+	colaIngresantes := TDACola.CrearColaEnlazada[TDAVotos.Votante]()
 
-    if !colaIngresantes.EstaVacia() {
-        err := new(errores.ErrorCiudadanosSinVotar)
-        fmt.Println(err.Error()) //habria que importar errores
-    }
+	contadorImpugnados := 0
+	//For que termina cuando finaliza entrada estandar
 
-    for i := 0; i < len(arrPartidos); i++ {
-        fmt.Println("Presidente:")
-        fmt.Println(arrPartidos[i].ObtenerResultado(TDAVotos.PRESIDENTE))
-    }
-    for i := 0; i < len(arrPartidos); i++ {
-        fmt.Println("Gobernador:")
-        fmt.Println(arrPartidos[i].ObtenerResultado(TDAVotos.GOBERNADOR))
-    }
-    for i := 0; i < len(arrPartidos); i++ {
-        fmt.Println("Intendente:")
-        fmt.Println(arrPartidos[i].ObtenerResultado(TDAVotos.INTENDENTE))
-    }
-    if contadorImpugnados == 1 {
-        fmt.Println("Votos Impugnados:", contadorImpugnados, "voto")
-    }else {
-        fmt.Println("Votos Impugnados:", contadorImpugnados, "votos")
-    }
+	//hace la llamada en cada linea, para analizar los comandos.
+	//Habria que definir su ComandosLeidos devuelve algo(un error) o es void
+	for scanner.Scan() {
+		linea := scanner.Text()
+		TDAOrdenYBusqueda.ComandosLeidos(linea, colaIngresantes, arrayVotantes, arrPartidos, &contadorImpugnados)
+	}
+
+	if !colaIngresantes.EstaVacia() {
+		err := new(errores.ErrorCiudadanosSinVotar)
+		fmt.Println(err.Error()) //habria que importar errores
+	}
+
+    fmt.Println("Presidente:")
+	for i := 0; i < len(arrPartidos); i++ {
+		fmt.Println(arrPartidos[i].ObtenerResultado(TDAVotos.PRESIDENTE))
+	}
+    fmt.Println("\nGobernador:")
+	for i := 0; i < len(arrPartidos); i++ {
+		fmt.Println(arrPartidos[i].ObtenerResultado(TDAVotos.GOBERNADOR))
+	}
+    fmt.Println("\nIntendente:")
+	for i := 0; i < len(arrPartidos); i++ {
+		fmt.Println(arrPartidos[i].ObtenerResultado(TDAVotos.INTENDENTE))
+	}
+	if contadorImpugnados == 1 {
+		fmt.Println("\nVotos Impugnados:", contadorImpugnados, "voto")
+	} else {
+		fmt.Println("\nVotos Impugnados:", contadorImpugnados, "votos")
+	}
 
 }
