647d450296a8db4e8be38d01a367cf29b7100048
NicoCoronelCaz <108557059+NicoCoronelCaz@users.noreply.github.com>
2023-05-08 12:08:29 -0300

    Update votante_implementacion.go

--- votos/votante_implementacion.go
+++ votos/votante_implementacion.go
@@ -28,11 +28,53 @@ func (votante *votanteImplementacion) Votar(tipo TipoVoto, alternativa int) erro
 		return errores.ErrorVotanteFraudulento{Dni: votante.dni}
 	}
 	//Aca creo un nuevo voto, copio el anterior y modifico lo que corresponde
-	voto := votante.votosRealizados.VerTope()
+    var voto Voto
+    if votante.votosRealizados.EstaVacia() {
+        voto = *new(Voto)
+    }else {
+        voto = votante.votosRealizados.VerTope()
+    }
+	
 	voto.VotoPorTipo[tipo] = alternativa
 	if alternativa == 0 {
         voto.Impugnado = true
     }
+	votante.votosRealizados.Apilar(voto)
+
+	return nil
+
+}
+
+func (votante *votanteImplementacion) Deshacer() error {
+    //Si ya voto
+    if votante.yaVoto {
+        votante.votosRealizados.Apilar(*new(Voto))
+        return errores.ErrorVotanteFraudulento{Dni: votante.dni}
+    }
+    //Si no hay votos, imprime el error
+    if votante.votosRealizados.EstaVacia() {
+        return errores.ErrorNoHayVotosAnteriores{}
+    }
+
+    //Elimino la version del ultimo voto
+    votante.votosRealizados.Desapilar()
+
+
+    return nil
+}
+
+func (votante *votanteImplementacion) FinVoto() (Voto, error) {
+    if votante.yaVoto {
+        votante.votosRealizados.Apilar(*new(Voto))
+        return Voto{}, errores.ErrorVotanteFraudulento{Dni: votante.dni}
+    }
+
+    votante.yaVoto = true
+    if votante.votosRealizados.EstaVacia() {
+        return *new(Voto), nil
+    }
+    return votante.votosRealizados.VerTope(), nil
+}
 	votante.votosRealizados.Apilar(voto)
 
 	return nil
